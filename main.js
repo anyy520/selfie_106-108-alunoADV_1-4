5979d2a588ba291f84f054daeaadcff5f17f1a4d
AnyySann <86086338+anyy520@users.noreply.github.com>
2023-12-05 15:02:26 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+// 1. Código JS para fala em texto:
+
+// 2. defina a função de início e escreva o código para ela:
+
+// 3. Este resultado é a conversão de nossa fala em texto
+
+// Escreveremos uma função speak() que realizará a conversão de texto para fala.
+
+
+    
+
+// Escreveremos o código para a funcionalidade da webcam no arquivo main.js, e adicionaremos o
+// código da webcam após a função speak() 
+
+
+
+
+// aula 100 - escreveremos o código para tirar uma selfie. Adicione este código após o código da webcam -> A função
+// takeSelfie() será a nossa função takeSelfie()
+
+
+// aula 100 -Já sabemos como escrever o código para um botão HTML e fornecê-lo uma função onclick.
+// Vejamos a função setTimeOutButton:
+
+
+// aula 100 - escreveremos o código para salvar a selfie tirada.
+// Escreva o código para isso, após a função takeSelfie(
